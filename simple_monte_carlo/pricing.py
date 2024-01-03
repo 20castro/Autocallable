9be697e3ca498169849ce756bf65e163839aafed
20castro <david.castro@etu.minesparis.psl.eu>
2024-01-03 18:06:22 +0100

    Correction

--- simple_monte_carlo/pricing.py
+++ simple_monte_carlo/pricing.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.stats import norm
-from pandas import value_counts
+import pandas as pd
 
 class MonteCarlo:
 
@@ -25,18 +25,22 @@ class MonteCarlo:
     def _barrier_breach_time(self, simulation: np.ndarray):
         breach = np.max(simulation, axis=0) >= self.barrier
         value_at_expiry = simulation[-1, np.logical_not(breach)]
-        first_breach = self.frequency*(1 + np.argmax(simulation[::, breach], axis=0))
+        first_breach = 1 + np.argmax(simulation[::, breach], axis=0)
         return first_breach, value_at_expiry
     
     def valuation(self, simulation: np.ndarray, verbose=True):
-        first_breach, value_at_expiry =  self._barrier_breach_time(simulation)
+        first_breach, value_at_expiry = self._barrier_breach_time(simulation)
         n_breach = len(first_breach)
         n_expire = len(value_at_expiry)
         n_put_exercised = np.count_nonzero(value_at_expiry <= self.strike)
         n = n_breach + n_expire
         if verbose:
             print(f'{(100*n_breach/n):.2f}% hit the barrier and the put is exercised {(100*n_put_exercised/n):.2f}% of the time')
-        discount_autocall = np.exp(-self.rate*first_breach)
+            first_breach_count = pd.value_counts(first_breach)/n
+            print('Barrier breach at time:')
+            for t, cnt in first_breach_count.sort_index().items():
+                print(f'- {t*self.frequency} years: {100*cnt:.2f}% of the time')
+        discount_autocall = np.exp(-self.rate*first_breach*self.frequency)
         payoff_in_case_of_breach = discount_autocall*self.notional*(1 + first_breach*self.coupon)
         discount_maturity = np.exp(-self.rate*self.maturity)
         put_payoff = np.maximum(0, self.strike - value_at_expiry)
